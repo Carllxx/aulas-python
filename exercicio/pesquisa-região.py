53cdafd22703b1f40f113e8020cba4cd77681097
Carllxx <159367323+Carllxx@users.noreply.github.com>
2024-04-18 19:52:02 +0000

    oii

---
+++ exercicio/pesquisa-região.py
@@ -0,0 +1,16 @@
+import os
+os.system ("clear")
+
+idade = int
+sexo = str
+escolha = str
+salario = float
+soma = 0
+media = 0
+
+while True:
+    print = ("=== MENO ===")
+    escolha = input ("1. adicionar \n
+                     2. exibir resultado\n")
+                     3. sair\n"
+                     R")
