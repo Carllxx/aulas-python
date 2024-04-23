2b025c2a8cf7b225eed7da655f65718ace668d7e
Carllxx <159367323+Carllxx@users.noreply.github.com>
2024-04-23 17:38:23 +0000

    Dark

---
+++ exercicio/vetor5.py
@@ -0,0 +1,14 @@
+import os
+os.system ("clear")
+
+valores = []
+
+for i in range (1,7):
+    valor = float (input(f"Insira {i} número par ou ímpar: "))
+    valores.append(valor)
+
+for i in range (6, 0, -1):
+    print (i)     
+
+
+
--- exercicio/ímpares-pares.py
+++ exercicio/ímpares-pares.py
@@ -6,13 +6,13 @@ soma = 0
 contador = 0
 while True:
     nota = float(input("Digite uma nota positva: "))
-    if nota > 0:
+    if nota >= 0:
         soma += nota
         contador += 1
-    if nota == 0:
+    if nota < 0:
         break
 print ("Calculando... ")
 media = soma / contador
 
 os.system("clear")
-print(f"Média Geral: {mediaGeral}")
+print(f"Média Geral: {media}")
