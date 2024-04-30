83ea7699e4b0206047850438c536481f19f91eb6
Carllxx <159367323+Carllxx@users.noreply.github.com>
2024-04-30 17:03:24 +0000

    Dark

---
+++ exercicio/24- Logo Senai.py
@@ -0,0 +1,23 @@
+import os
+
+#função sem retorno
+def logoSenai ():
+    os.system
+    print("=== ===== ===")
+    print("=== SENAI ===")
+    print("=== ===== ===")
+#solicitando dados de usuario.
+logoSenai()
+nome = input("Digite seu nome: ")
+
+logoSenai()
+idade = int(input("Informe a sua idade: "))
+
+logoSenai ()
+peso = float(input("Digite seu peso: "))
+
+#Exibindo dados de usuario... 
+logoSenai()
+print(f"Nome: {nome}")
+print(f"Idade {idade}")
+print(f"Peso {peso}")
