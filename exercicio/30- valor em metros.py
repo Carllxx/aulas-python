e595254efd37c8ef962deac791e401f92f4f38ad
Carllxx <159367323+Carllxx@users.noreply.github.com>
2024-04-30 19:24:55 +0000

    Dark

---
+++ exercicio/30- valor em metros.py
@@ -0,0 +1,17 @@
+import os
+
+def metros ():
+    os.system
+    print("=== DARK ===")
+
+def cent (m):
+    resultado = m * 100
+    return resultado
+
+metros()
+metros = int(input("Digite um valor em metros: "))
+cent(metros)
+
+resultado = cent(metros)
+
+print(f"o metro convertido por centimetros: {resultado}")
