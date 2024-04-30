dee2954abff896d0af7b5953a881bc1bbec73a43
Carllxx <159367323+Carllxx@users.noreply.github.com>
2024-04-30 17:12:34 +0000

    Dark

---
+++ exercicio/25- logo Doceria.py
@@ -0,0 +1,21 @@
+import os
+
+#função sem retorno.
+def logoDoceria ():
+    os.system
+    print("=== Doceria ===")
+
+#função sem retorno.
+def somar(n1,n2):
+    resultado = n1 + n2
+    return resultado
+
+#solicitando dados para usuario...
+logoDoceria()
+primeiroNumero = int(input("Digite o primeiro numero: "))
+segundoNumero = int (input("Digite o segundo numero: "))
+
+soma = somar(primeiroNumero , segundoNumero)
+
+#Exibindo dados para usuário.
+print(f"Soma: {soma}")
