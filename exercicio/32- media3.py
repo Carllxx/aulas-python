24d7a8fa8a3065e33a893ae71f6650bf62d64012
Carllxx <159367323+Carllxx@users.noreply.github.com>
2024-04-30 19:57:38 +0000

    Dark

---
+++ exercicio/32- media3.py
@@ -0,0 +1,16 @@
+import os
+
+def media(n1,n2):
+    soma = n1 + n2
+    media = soma / 2
+    return media
+
+if media >=7:
+    situaqção = 'Aprovado'
+else:
+    situaqção = 'Reprovado'
+
+# 30/04/2024 > 16:57
+
+
+
